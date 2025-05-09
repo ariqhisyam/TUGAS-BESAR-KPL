2c6688bafe22beec579bf523130d7b1ec51a52ad
aditya-0122 <aaaaadit2021@gmail.com>
2025-05-09 15:46:23 +0700

    push project

--- TUGASBESAR_kelompok_SagaraDailyCheckUp/obj/Debug/net8.0/TUGASBESAR_kelompok_SagaraDailyCheckUp.AssemblyInfo.cs
+++ TUGASBESAR_kelompok_SagaraDailyCheckUp/obj/Debug/net8.0/TUGASBESAR_kelompok_SagaraDailyCheckUp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TUGASBESAR_kelompok_SagaraDailyCheckUp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca4cdf9e09c5dc8fc0160681d17bb50e22ff5d43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85009d032d163d5922271f72130095eb1f44e233")]
 [assembly: System.Reflection.AssemblyProductAttribute("TUGASBESAR_kelompok_SagaraDailyCheckUp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TUGASBESAR_kelompok_SagaraDailyCheckUp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
