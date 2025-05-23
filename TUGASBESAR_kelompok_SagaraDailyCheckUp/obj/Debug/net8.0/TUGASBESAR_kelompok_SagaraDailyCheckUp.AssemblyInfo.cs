27f14ce6311ba0f3ee5fa96e0820dae058cf2775
relinggaa <linggayorfel@gmail.com>
2025-05-23 12:39:11 +0700

    merge from try merge

--- TUGASBESAR_kelompok_SagaraDailyCheckUp/obj/Debug/net8.0/TUGASBESAR_kelompok_SagaraDailyCheckUp.AssemblyInfo.cs
+++ TUGASBESAR_kelompok_SagaraDailyCheckUp/obj/Debug/net8.0/TUGASBESAR_kelompok_SagaraDailyCheckUp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TUGASBESAR_kelompok_SagaraDailyCheckUp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b77cdc1619d0fb5473360ea54d800e371a1eeb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c92c210437df61dd7369f6c8b102ad5739725171")]
 [assembly: System.Reflection.AssemblyProductAttribute("TUGASBESAR_kelompok_SagaraDailyCheckUp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TUGASBESAR_kelompok_SagaraDailyCheckUp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
